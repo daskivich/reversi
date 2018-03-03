09158be73d0f74f6ab76cf68c219dac209c09286
Dan Daskivich <daskivich.d@husky.neu.edu>
2018-03-03 18:32:07 -0500

    Created phoenix project.

---
+++ test/reversi_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule ReversiWeb.ErrorViewTest do
+  use ReversiWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(ReversiWeb.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(ReversiWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(ReversiWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
